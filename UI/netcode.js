4d7bd4826b16bed8686951915caf3e20278ab749
Brenden Stahle <42918416+Brenden2008@users.noreply.github.com>
2023-01-22 19:25:47 -0600

    Some updates

--- UI/netcode.js
+++ UI/netcode.js
@@ -19,35 +19,53 @@ const hbnet = {
             try {
                 this.room = joinRoom(baseConfig, this.boardID.toString())
             } catch {
-                return 'meiyou'
+                alert("Couldn't connect to the HBL network!")
+                throw new Error("Couldn't connect to the HBL network!");
             }
             // Handle Debug
             if(this.debug == true){
-                room.onPeerJoin(peerId => console.log(`${peerId} joined`))
-                room.onPeerLeave(peerId => console.log(`${peerId} left`))
+                this.room.onPeerJoin(peerId => console.log(`${peerId} joined`))
+                this.room.onPeerLeave(peerId => console.log(`${peerId} left`))
             }
             // Initialize Actions
             const [submitConnectForm, receiveConnectForm] = this.room.makeAction('connectForm')
-            const [sendName, getName] = room.makeAction('name')
+            const [sendName, getName] = this.room.makeAction('name')
             const idsToNames = {}
+            function inverse(obj){
+                var retobj = {};
+                for(var key in obj){
+                  retobj[obj[key]] = key;
+                }
+                return retobj;
+            }
             // Handle Actions
-            room.onPeerJoin(peerId => sendName('87ab8a8b-35eb-45b8-a826-c9babdbbcdf8', peerId))
-            sendName('87ab8a8b-35eb-45b8-a826-c9babdbbcdf8')
             getName((name, peerId) => (idsToNames[peerId] = name))
+
+            // Check for existing admins before declaring ourself as admin
+            // Wait 5 seconds for an admin to announce their presence
+            setTimeout(function(){
+                if (inverse(idsToNames)['87ab8a8b-35eb-45b8-a826-c9babdbbcdf8'] != null) {
+                    alert("Admin Already Exists!")
+                    throw new Error("Admin Already Exists!");
+                } else {
+                    this.room.onPeerJoin(peerId => sendName('87ab8a8b-35eb-45b8-a826-c9babdbbcdf8', peerId))
+                    sendName('87ab8a8b-35eb-45b8-a826-c9babdbbcdf8')
+                }
+            }, 5000);
             receiveConnectForm((data, peerId) => {
                 if(this.debug == true){
                     console.log(
                         `Received A connectForm!
                          peerId = ${peerId}
                          formalName = ${idsToNames[peerId] || 'a weird stranger'}
-                         data = ${JSON.stringify(data)}`
+                         data = ${data}`
                     )
                 }
                 this.dbconnectForm.put({
                     _id: Math.floor(10000000 + Math.random() * 90000000),
                     peerId: peerId,
                     formalName: idsToNames[peerId] || 'a weird stranger',
-                    data: JSON.stringify(data),
+                    data: data,
                 });
             })
         }
@@ -66,16 +84,17 @@ const hbnet = {
             try {
                 this.room = joinRoom(baseConfig, this.boardID.toString())
             } catch {
-                return 'meiyou'
+                alert("Couldn't connect to the HBL network!")
+                throw new Error("Couldn't connect to the HBL network!");
             }
             // Handle Debug
             if(this.debug == true){
-                room.onPeerJoin(peerId => console.log(`${peerId} joined`))
-                room.onPeerLeave(peerId => console.log(`${peerId} left`))
+                this.room.onPeerJoin(peerId => console.log(`${peerId} joined`))
+                this.room.onPeerLeave(peerId => console.log(`${peerId} left`))
             }
             // Initialize Actions
             const [submitConnectForm, receiveConnectForm] = this.room.makeAction('connectForm')
-            const [sendName, getName] = room.makeAction('name')
+            const [sendName, getName] = this.room.makeAction('name')
             function inverse(obj){
                 var retobj = {};
                 for(var key in obj){
@@ -85,7 +104,7 @@ const hbnet = {
             }
             const idsToNames = {}
             // Handle Actions
-            room.onPeerJoin(peerId => sendName(name, peerId))
+            this.room.onPeerJoin(peerId => sendName(name, peerId))
             sendName(name)
             getName((name, peerId) => (idsToNames[peerId] = name))
             submitConnectForm(this.data, inverse(idsToNames)['87ab8a8b-35eb-45b8-a826-c9babdbbcdf8'])
