e0966a952873af94e0aa816f347dc5cb9f4165ec
Brenden Stahle <42918416+Brenden2008@users.noreply.github.com>
2023-01-22 21:21:26 -0600

    Import Functions

--- UI/netcode.js
+++ UI/netcode.js
@@ -18,7 +18,7 @@ const hbnet = {
         var boards = new PouchDB('boards');
         boards.put({
             _id: this.boardID.toString(),
-            boardID: this.boardID,
+            boardID: this.boardID.toString(),
         });
 
         this.host = function(){
