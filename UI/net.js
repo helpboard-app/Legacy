c56ae21f7d9fce9b546cb846e1a29bae5431540e
Brenden Stahle <42918416+Brenden2008@users.noreply.github.com>
2023-01-28 16:23:13 -0600

    Ok lets check if this works

--- UI/net.js
+++ UI/net.js
@@ -12,6 +12,7 @@ const net = {
         this.boardid = boardid
 
         this.boardsdb.get(boardid.toString()).then(function (doc) {
+            cb({status: "initalized"})
           }).catch(function (err) {
             var boardsdb = new PouchDB('boards');
             if (err.status === 404) {
@@ -24,16 +25,18 @@ const net = {
                 })
             } else {
               console.log('Error checking for document:', err);
+              cb({status: "failedinit"})
               //this.statusHandler({status: false, err: `Error checking for document: ${err}`})
             }
         });
     },
-    Client: function({boardid}){
+    Client: function({boardid, cb}){
         this.statusHandler = function(status){
             return true;
         }
         this.network = joinRoom(baseConfig, boardid.toString())
         this.boardid = boardid
+        cb({status: "initalized"})
     }
 }
 
