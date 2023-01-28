7203d144572d0ad886c1eeffe4a2feeb403e397b
Brenden Stahle <42918416+Brenden2008@users.noreply.github.com>
2023-01-27 21:58:41 -0600

    The new codebase is ~60% smaller and includes a form renderer

---
+++ UI/net.js
@@ -0,0 +1,36 @@
+import {joinRoom} from 'https://cdn.skypack.dev/trystero'
+const baseConfig = {appId: 'HelpboardLegacy'}
+
+const net = {
+    Board: function({boardid}){
+        this.statusHandler = function(status){
+            return true;
+        }
+        this.db = new PouchDB('board' + boardid);
+        this.boardsdb = new PouchDB('boards');
+        this.network = joinRoom(baseConfig, boardid.toString())
+        this.boardid = boardid
+
+        this.boardsdbdb.get(boardid).then(function (doc) {
+          }).catch(function (err) {
+            if (err.status === 404) {
+                this.db.put({
+                    _id: boardid.toString(),
+                    boardID: boardid
+                });
+            } else {
+              console.log('Error checking for document:', err);
+              this.statusHandler({status: false, err: `Error checking for document: ${err}`})
+            }
+        });
+    },
+    Client: function(boardid){
+        this.statusHandler = function(status){
+            return true;
+        }
+        this.network = joinRoom(baseConfig, boardid.toString())
+        this.boardid = boardid
+    }
+}
+
+export {net}
